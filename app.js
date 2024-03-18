5c81e174989fc9449e153c2a7fc9ceaacb42a885
jimena2012 <163925876+jimena2012@users.noreply.github.com>
2024-03-18 15:15:43 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados =[];
+let numeroMaximo = 10;
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    if (numeroDeUsuario == numeroSecreto){
+        asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${(intentos === 1) ? 'vez':'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else{
+        //El usuario no acerto
+        if(numeroDeUsuario>numeroSecreto){
+            asignarTextoElemento('p','El numero secreto es menor');
+        } else {
+            asignarTextoElemento('p','El numero secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja()
+    }
+    return;
+}
+
+function limpiarCaja(){
+   document.querySelector('#valorUsuario').value = ' ';
+
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+    if(listaNumerosSorteados.length==numeroMaximo){
+        asignarTextoElemento('p','Ya se sorteron todos los numeros posibles');
+    }else{
+        if (listaNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+        } else{
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function reiniciarJuego() {
+    //limpiar la caja
+    limpiarCaja();
+    //indicar mensaje de intervalo
+    //Generar numero aleatorio
+    //Deshabilitar el boton de nuevo juego
+    document.getElementById('reiniciar').setAttribute('disabled','true');
+    //Inicializar intentos
+    condicionesIniciales();
+    
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del numero secreto');
+    asignarTextoElemento('p',`Indica un numero del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+condicionesIniciales();
